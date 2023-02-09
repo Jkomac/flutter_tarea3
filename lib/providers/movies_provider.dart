3c18e1ca71dffca55de4b8e38d8d8b3e8636f186
Jkomac <j.se19@hotmail.com>
2023-02-09 11:51:50 +0100

    first commit

---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import '../models/models.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  // Atributos
+  String _baseUrl = 'api.themoviedb.org'; // No hace falta todo se puede copiar el contenido tras https:// hasta el dominio principal
+  String _apiKey = 'bdf025da16866f6d864b4124114642c2';
+  String _language = 'es-ES';
+  String _page = '1';
+
+  List<Movie> onDisplayMovies = [];
+
+  MoviesProvider() {
+    print('Movies Provider inicializado');
+    this.getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async { // Metodo encargado de hacer la peticion al servidor para obtener la info de las pelis
+    print('getOnDisplayMovies');
+    // (URL, Acceder al EndPoint, Definir conjunto Clave - Valor)
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': _page
+    }); 
+
+    // Await the http get response, then decode the json-formatted response.
+    final result = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(result.body);
+
+    onDisplayMovies = nowPlayingResponse.results; // Iniciamos la variable con el listado de peliculas
+
+    notifyListeners(); // Avisar a todo el arbol de Widgets que usan este provider de que ha habido cambios
+  }
+
+  // getOnPopulars() async {} + consulta
+}
---
+++ lib/widgets/card_swiper.dart
@@ -0,0 +1,55 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/material.dart';
+import 'package:movies_app/models/models.dart';
+
+class CardSwiper extends StatelessWidget {
+
+  final List<Movie> movies;
+
+  // Constructor
+  const CardSwiper({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    if(this.movies.isEmpty){
+      return Container(
+        width: double.infinity,
+        height: size.height * 0.5,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Container(
+        width: double.infinity,
+        // Aquest multiplicador estableix el tant per cent de pantalla ocupada 50%
+        height: size.height * 0.5,
+        // color: Colors.red,
+        child: Swiper(
+          itemCount: movies.length,
+          layout: SwiperLayout.STACK,
+          itemWidth: size.width * 0.6,
+          itemHeight: size.height * 0.4,
+          itemBuilder: (BuildContext context, int index) {
+            final movie = movies[index];
+            // print(movie.posterPath);
+            return GestureDetector(
+              onTap: () => Navigator.pushNamed(context, 'details',
+                  arguments: 'detalls peli'),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                    placeholder: AssetImage('assets/no-image.jpg'), // Imagen de carga 
+                    image: NetworkImage(movie.fullPosterPath),
+                    fit: BoxFit.cover),
+              ),
+            );
+          },
+        ));
+  }
+}
+
+// URL para visualizar portadas: https://image.tmdb.org/t/p/w500
